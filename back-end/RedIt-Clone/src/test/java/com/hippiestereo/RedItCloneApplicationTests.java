5b14f2b6fa0444a407bbf3af4099d3595a5a1fef
HippieStereo <stereobroo@yahoo.com>
2020-09-23 17:05:40 +0100

    BE - Create -> Spring project

---
+++ back-end/RedIt-Clone/src/test/java/com/hippiestereo/RedItCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hippiestereo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedItCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
