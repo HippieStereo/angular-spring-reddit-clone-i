1ff12a969e237c57a1598ce904defcda849a2883
HippieStereo <stereobroo@yahoo.com>
2020-09-24 15:32:37 +0100

    Clean - Change app name

--- back-end/Reddit-Clone/src/main/java/com/hippiestereo/RedditCloneApplication.java
+++ back-end/Reddit-Clone/src/main/java/com/hippiestereo/RedditCloneApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedItCloneApplication {
+public class RedditCloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RedItCloneApplication.class, args);
+		SpringApplication.run(RedditCloneApplication.class, args);
 	}
 
 }
