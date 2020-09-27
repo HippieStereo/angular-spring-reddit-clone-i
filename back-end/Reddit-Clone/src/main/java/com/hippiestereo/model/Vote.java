62dd57d33bc545845a31c53a9f996ab2e94e700f
HippieStereo <stereobroo@yahoo.com>
2020-09-27 12:10:57 +0100

    BE - Model
    Update -> Vote

--- back-end/Reddit-Clone/src/main/java/com/hippiestereo/model/Vote.java
+++ back-end/Reddit-Clone/src/main/java/com/hippiestereo/model/Vote.java
@@ -26,4 +26,32 @@ public class Vote {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "postId", referencedColumnName = "postId")
 	private User user;
+
+	public VoteType getVoteType() {
+		return voteType;
+	}
+
+	public void setVoteType(VoteType voteType) {
+		this.voteType = voteType;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Long getVoteId() {
+		return voteId;
+	}
 }
