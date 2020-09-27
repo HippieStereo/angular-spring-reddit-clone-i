491e216a1b1b816fb6d5f7fe6f70207ec67e2201
HippieStereo <stereobroo@yahoo.com>
2020-09-27 11:59:08 +0100

    BE - Model
    Create -> VerificationToken

---
+++ back-end/Reddit-Clone/src/main/java/com/hippiestereo/model/VerificationToken.java
@@ -0,0 +1,54 @@
+package com.hippiestereo.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "spring-reddit-clone-token")
+public class VerificationToken {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String token;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	private User user;
+	
+	private Instant expiryDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Instant getExpiryDate() {
+		return expiryDate;
+	}
+}
