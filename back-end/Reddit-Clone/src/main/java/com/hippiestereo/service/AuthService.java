80065f2ced8e0d0fb1c173a80c60107329f21451
HippieStereo <stereobroo@yahoo.com>
2020-10-02 16:43:45 +0100

    Update AuthService.java

--- back-end/Reddit-Clone/src/main/java/com/hippiestereo/service/AuthService.java
+++ back-end/Reddit-Clone/src/main/java/com/hippiestereo/service/AuthService.java
@@ -8,28 +8,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hippiestereo.dto.RegisterRequest;
+import com.hippiestereo.model.NotificationEmail;
 import com.hippiestereo.model.User;
 import com.hippiestereo.model.VerificationToken;
 import com.hippiestereo.repository.UserRepository;
 import com.hippiestereo.repository.VerificationTokenRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class AuthService {
 	
 	private final PasswordEncoder passwordEncoder;
 	private final VerificationTokenRepository verificationTokenRepository;
 	private final UserRepository userRepository;
 	private final MailService mailService;
-	
-	public AuthService(PasswordEncoder passwordEncoder, 
-			UserRepository userRepository, 
-			VerificationTokenRepository verificationTokenRepository,
-			MailService mailService) {
-		this.passwordEncoder = passwordEncoder;
-		this.userRepository = userRepository;
-		this.verificationTokenRepository = verificationTokenRepository;
-		this.mailService = mailService;
-	}
+	//private final AuthenticationManager authenticationManager;
+	//private final JwtProvider jwtProvider;
+	//private final RefreshTokenService refreshTokenService;
 	
 	@Transactional
 	public void signup(RegisterRequest registerRequest) {
@@ -44,6 +41,11 @@ public class AuthService {
 		userRepository.save(user);
 		
 		String token = generateVerificationToken(user);
+		
+		mailService.sendmail(new NotificationEmail("Please Activate your Account",
+                user.getEmail(), "Thank you for signing up to Spring Reddit, " +
+                "please click on the below url to activate your account : " +
+                "http://localhost:8080/api/auth/accountVerification/" + token));
 	}
 
 	private String generateVerificationToken(User user) {
