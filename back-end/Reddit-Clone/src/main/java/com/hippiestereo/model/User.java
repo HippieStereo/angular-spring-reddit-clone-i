0d933c5e788f54e879015e1d43d9d380c74f90df
HippieStereo <stereobroo@yahoo.com>
2020-10-02 16:43:04 +0100

    Update User.java
    BE

--- back-end/Reddit-Clone/src/main/java/com/hippiestereo/model/User.java
+++ back-end/Reddit-Clone/src/main/java/com/hippiestereo/model/User.java
@@ -10,9 +10,11 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class User {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
@@ -29,38 +31,4 @@ public class User {
 	
 	private Instant created;
 	private boolean enabled;
-	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Instant getCreated() {
-		return created;
-	}
-	public void setCreated(Instant created) {
-		this.created = created;
-	}
-	public boolean isEnabled() {
-		return enabled;
-	}
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-	public Long getUserId() {
-		return userId;
-	}
 }
