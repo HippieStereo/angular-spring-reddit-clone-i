ebb030e43529cebb8564e75faf197e1efba631bc
Hippie Stereo <stereobroo@yahoo.com>
2020-10-19 17:25:33 +0100

    FE -> Login + Toast Message

--- front-end/bootstrap/angular-bootstrap-reddit-clone/src/app/app-routing.module.ts
+++ front-end/bootstrap/angular-bootstrap-reddit-clone/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { from } from 'rxjs';
 import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-  {path: 'sign-up', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- front-end/bootstrap/angular-bootstrap-reddit-clone/src/app/auth/shared/auth.service.ts
+++ front-end/bootstrap/angular-bootstrap-reddit-clone/src/app/auth/shared/auth.service.ts
@@ -1,18 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { SignupRequestPayload } from '../signup/signup-request.payload';
 import { Observable } from 'rxjs';
+import { LoginRequestPayload } from '../login/login-request.payload';
+import { LoginResponse } from '../login/login-response.payload';
+import { LocalStorageService } from 'ngx-webstorage';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  @Output() loggedIn: EventEmitter<boolean> = new EventEmitter();
+  @Output() username: EventEmitter<string> = new EventEmitter();
+
+  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) { }
 
   signup(signupRequestPayload : SignupRequestPayload): Observable<any> {
 
     return this.httpClient.post('http://localhost:8080/api/auth/signup', signupRequestPayload, {responseType: 'text'});
+
+  }
+
+  login(loginRequestPayload: LoginRequestPayload){
+
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequestPayload).pipe(
+      map(data => {
+        this.localStorage.store('authenticationToken', data.authenticationToken);
+        this.localStorage.store('username', data.username);
+        this.localStorage.store('refreshToken', data.refreshToken);
+        this.localStorage.store('expiresAt', data.expiresAt);
+
+        this.loggedIn.emit(true);
+        this.username.emit(data.username);
+        
+        return true;
+      })
+    );
 
   }
 
